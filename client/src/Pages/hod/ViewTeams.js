6bee36f6327c50f9cf8cb66cd05ff659249746f4
KamaliHari <kamalihari2002@gmail.com>
2023-08-28 14:46:58 +0530

    viewteams

--- client/src/Pages/hod/ViewTeams.js
+++ client/src/Pages/hod/ViewTeams.js
@@ -1,18 +1,22 @@
 import React, { useState,useEffect } from 'react';
-import { getDocs,collection, doc, getDoc } from 'firebase/firestore';
+import { getDocs,collection, doc, getDoc,updateDoc } from 'firebase/firestore';
 import { db } from '../../firebaseConfig';
 import TeamCard from './TeamCards';
 import Sidebar from '../../Components/Sidebar';
-import {Card} from "antd";
+import {Button, Card,Select} from "antd";
 
 
 export default function ViewTeams() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
+  const { Option } = Select;
+  const [mentors, setMentors] = useState([]);
+  const [selectedMentor, setSelectedMentor] = useState('');
+
   useEffect(() => {
     const fetchData = async () => {
       
-      const collectionRef = collection(db, 'Team_Details'); // Replace with your collection name
+      const collectionRef = collection(db, 'Team_Details');
 
       try {
         const querySnapshot = await getDocs(collectionRef);
@@ -30,7 +34,55 @@ export default function ViewTeams() {
     };
 
     fetchData();
+
+    const fetchMentors = async () => {
+    
+      const mentorsCollectionRef = collection(db, 'Staffs');
+
+      try {
+        const querySnapshot = await getDocs(mentorsCollectionRef);
+        const mentorList = querySnapshot.docs.map((doc) => doc.data());
+        setMentors(mentorList);
+      } catch (error) {
+        console.error('Error fetching mentors:', error);
+      }
+    };
+
+    fetchMentors();
   }, []);
+  
+
+  // const handleMentorChange = async (userid, selectedMentor) => {
+    
+  //   const teamDocRef = doc(db, 'Team_Details', userid);
+
+  //   try {
+  //     await updateDoc(teamDocRef, {
+  //       mentor: selectedMentor
+  //     });
+  //   } catch (error) {
+  //     console.error('Error updating mentor:', error);
+  //   }
+  // };
+  const handleUpdateMentor = async (userId, mentor) => {
+    if (!mentor) {
+      console.log('Select a mentor before setting.');
+      return;
+    }
+
+   
+    const teamDocRef = doc(db, 'Team_Details', userId); 
+
+    try {
+      await updateDoc(teamDocRef, {
+        mentor: mentor
+      });
+      console.log(`Mentor set to: ${mentor}`);
+    } catch (error) {
+      console.error('Error updating mentor:', error);
+    }
+  };
+  
   if (loading) {
     return <div>Loading...</div>; 
   }
@@ -50,20 +102,18 @@ export default function ViewTeams() {
                
               <p><strong>Mail ID:</strong> {doc.Team_Leader_Mailid}</p>
               <p><strong>Registration Number:</strong> {doc.Team_Member1_Regnum}</p>
-              
-            </Card>
-          ))}
-          </div>
-    </div>
-
-    <div className="team-column">
-    <div className="team-cards">
-    {data.map((doc, index) => (
-            <Card key={index} title={`Team Leader: ${doc.Team_Leader_firstname}`}>
-             
-              <p><strong>Mail ID:</strong> {doc.Team_Leader_Mailid}</p>
-              <p><strong>Registration Number:</strong> {doc.Team_Member1_Regnum}</p>
-             
+              <Select
+                placeholder="Select a mentor"
+                onChange={(selectedMentor) => setSelectedMentor(selectedMentor)}
+                style={{ width: '100%' }}
+              >
+                {mentors.map((mentor, index) => (
+                  <Option key={index} value={mentor.Name}>
+                    {mentor.Name}
+                  </Option>
+                ))}
+              </Select>
+              <Button type="primary" onClick={() => handleUpdateMentor(doc.id, selectedMentor)}>Set Mentor</Button>
             </Card>
           ))}
           </div>
