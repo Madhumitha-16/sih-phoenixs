b3d9fa6afbdd57ccacb324aebdbc9f595590f006
KamaliHari <kamalihari2002@gmail.com>
2023-08-29 22:02:46 +0530

    view teams done

--- client/src/Pages/hod/ViewTeams.js
+++ client/src/Pages/hod/ViewTeams.js
@@ -1,58 +1,62 @@
 import React, { useState,useEffect } from 'react';
 import { getDocs,collection, doc, getDoc,updateDoc, Firestore, query, where } from 'firebase/firestore';
 import { db } from '../../firebaseConfig';
-import {Button, Card,Select} from "antd";
+import {Button, Card,Select,} from "antd";
+const { Option } = Select;
 
 
 export default function ViewTeams() {
-  const [teamData, setTeamData] = useState({});
   const [loading, setLoading] = useState(true);
-  const { Option } = Select;
+  const [teamLeaders, setTeamLeaders] = useState([]); 
   const [mentors, setMentors] = useState([]);
   const [selectedMentor, setSelectedMentor] = useState('');
-  const [Data, setData] = useState([]);
+  
  
 
   useEffect(() => {
+
     const fetchData = async () => {
       try {
-        const collection1Ref = collection(db, 'Team_Details');
-        const collection2Ref = collection(db, 'PhaseI');
-    
-        const collection1Snapshot = await getDocs(collection1Ref);
-        const collection2Snapshot = await getDocs(collection2Ref);
-    
-        const combinedData = [];
-    
-        const matchingUserIds = collection1Snapshot.docs
-          .map(doc => doc.data().userid)
-          .filter(userid => collection2Snapshot.docs.some(d => d.data().userid === userid));
-    
-        collection1Snapshot.docs.forEach(doc => {
-          const docData = doc.data();
-          if (matchingUserIds.includes(docData.userid)) {
-            const matchingDoc = collection2Snapshot.docs.find(d => d.data().userid === docData.userid);
-            if (matchingDoc) {
-              combinedData.push({
-                userid: docData.userid,
-                Team_Leader_firstname: docData.Team_Leader_firstname,
-                Team_Leader_Mailid: docData.Team_Leader_Mailid,
-                Team_Leader_Regnum: docData.Team_Leader_Regnum,
-                Project_Title: matchingDoc.data().Project_Title,
-                Domain: matchingDoc.data().Domain,
-              });
-            }
+        const teamDetailsRef = collection(db, 'Team_Details');
+        const phaseIRef = collection(db, 'PhaseI');
+
+        const teamDetailsSnapshot = await getDocs(teamDetailsRef);
+        const phaseISnapshot = await getDocs(phaseIRef);
+
+        const teamDetailsData = teamDetailsSnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        const combinedData = teamDetailsData.map(teamLeader => {
+          const phaseIDoc = phaseISnapshot.docs.find(phaseI => phaseI.data().userid === teamLeader.userid);
+          if (phaseIDoc) {
+            return {
+              ...teamLeader,
+              Project_Title: phaseIDoc.data().Project_Title,
+              Domain: phaseIDoc.data().Domain
+            };
+          } else {
+            return {
+              ...teamLeader,
+              Project_Title: 'NA',  // Set to 'NA' if not submitted
+              Domain: 'NA'          // Set to 'NA' if not submitted
+            };
           }
         });
-    
-        setData(combinedData); // Set the Data state with combinedData
+
+        setTeamLeaders(combinedData);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-    
+
     fetchData();
+  
+
+
+  
     const fetchMentors = async () => {
     
       const mentorsCollectionRef = collection(db, 'Staffs');
@@ -68,37 +72,40 @@ export default function ViewTeams() {
     };
 
     fetchMentors();
+    
 
   }, []);
 
-  
   //button func
-  
-  const handleUpdateMentor = async ( mentor) => {
-    if (!mentor) {
+  const handleUpdateMentor = async (userId, mentorName) => {
+    if (!mentorName) {
       console.log('Select a mentor before setting.');
       return;
     }
-    const teamDocRef = doc(db, 'Team_Details'); 
 
-    try {
-      await updateDoc(teamDocRef, {
-        Mentor: mentor
-      });
-      console.log(`Mentor set to: ${mentor}`);
-    } catch (error) {
-      console.error('Error updating mentor:', error);
+    const teamQueryRef = query(collection(db, 'Team_Details'), where('userid', '==', userId));
+    const teamQuerySnapshot = await getDocs(teamQueryRef);
+
+    if (!teamQuerySnapshot.empty) {
+      const teamDocRef = teamQuerySnapshot.docs[0].ref;
+
+      try {
+        await updateDoc(teamDocRef, {
+          Mentor: mentorName
+        });
+        console.log(`Mentor set to: ${mentorName}`);
+      } catch (error) {
+        console.error('Error updating mentor:', error);
+      }
     }
-    
   };
+
+
+  
   
   if (loading) {
     return <div>Loading...</div>; 
   }
-  
-
- 
-
 
   return (<>
     <div className="bodyWrap dashboardPage">
@@ -108,30 +115,35 @@ export default function ViewTeams() {
     <div className="team-column">
     <div className="team-cards">
    
-    {Data.map(item => (
-        <Card key={item} title={`Team Leader: ${item.Team_Leader_firstname}`}>
+    {teamLeaders.map(teamLeader => (
+               <Card key={teamLeader.id} title={`Team Leader: ${teamLeader.Team_Leader_firstname}`}>
+               <p>Mail ID: {teamLeader.Team_Leader_Mailid}</p>
+               <p>Registration Number: {teamLeader.Team_Leader_Regnum}</p>
+               <p>Project Title: {teamLeader.Project_Title}</p>
+               <p>Domain: {teamLeader.Domain}</p>
          
-        <p>Registration Number: {item.Team_Leader_Regnum}</p>
-        <p>Mail ID: <br></br>{item.Team_Leader_Mailid}</p>
-        <p>Project Title: {item.Project_Title}</p>
-        <p>Domain: {item.Domain}</p>
-              <Select
-                placeholder="Select a mentor"
-                onChange={(selectedMentor) => setSelectedMentor(selectedMentor)}
-                style={{ width: '100%',marginBottom:'30px' }}
-              >
-                {mentors.map((mentor, index) => (
-                  <Option key={index} value={mentor.Name}>
-                    {mentor.Name}
-                  </Option>
-                ))}
-              </Select>
-              <div className='buttonmentor'>
-              <Button className='buttonmentor' type="primary" onClick={() => handleUpdateMentor(doc.id, selectedMentor)}>Set Mentor</Button>
-              </div>
-            </Card>
-          ))}
-          </div>
+               <Select
+                 placeholder="Select a mentor"
+                 onChange={(selectedMentor) => setSelectedMentor(selectedMentor)}
+                 style={{ width: '100%', marginBottom: '15px' }}
+               >
+                 {mentors.map(mentor => (
+                   <Option key={mentor.id} value={mentor.Name}>
+                     {mentor.Name}
+                   </Option>
+                 ))}
+               </Select>
+         
+               <Button
+                 type="primary"
+                 onClick={() => handleUpdateMentor(teamLeader.userid,selectedMentor)}
+               >
+                 Set Mentor
+               </Button>
+                
+             </Card>
+            ))}
+   </div>
     </div>
   </div>
   </div>
