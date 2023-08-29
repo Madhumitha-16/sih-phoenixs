eef543359b2c322303cdca939be00267179dfaf2
KamaliHari <kamalihari2002@gmail.com>
2023-08-29 10:49:45 +0530

    view teams fetch and show done

--- client/src/Pages/hod/ViewTeams.js
+++ client/src/Pages/hod/ViewTeams.js
@@ -10,29 +10,48 @@ export default function ViewTeams() {
   const { Option } = Select;
   const [mentors, setMentors] = useState([]);
   const [selectedMentor, setSelectedMentor] = useState('');
-  const[prjData,setPrjData]=useState(''); 
+  const [Data, setData] = useState([]);
+ 
 
   useEffect(() => {
     const fetchData = async () => {
-      const collectionRef = collection(db, 'Team_Details');
       try {
-        const querySnapshot = await getDocs(collectionRef);
-        const userIDs = querySnapshot.docs.map((doc) => doc.data().userid);
-        
-        const documents = [];
-        querySnapshot.forEach((doc) => {
-        documents.push({ id: doc.id, ...doc.data() });
+        const collection1Ref = collection(db, 'Team_Details');
+        const collection2Ref = collection(db, 'PhaseI');
+    
+        const collection1Snapshot = await getDocs(collection1Ref);
+        const collection2Snapshot = await getDocs(collection2Ref);
+    
+        const combinedData = [];
+    
+        const matchingUserIds = collection1Snapshot.docs
+          .map(doc => doc.data().userid)
+          .filter(userid => collection2Snapshot.docs.some(d => d.data().userid === userid));
+    
+        collection1Snapshot.docs.forEach(doc => {
+          const docData = doc.data();
+          if (matchingUserIds.includes(docData.userid)) {
+            const matchingDoc = collection2Snapshot.docs.find(d => d.data().userid === docData.userid);
+            if (matchingDoc) {
+              combinedData.push({
+                userid: docData.userid,
+                Team_Leader_firstname: docData.Team_Leader_firstname,
+                Team_Leader_Mailid: docData.Team_Leader_Mailid,
+                Team_Leader_Regnum: docData.Team_Leader_Regnum,
+                Project_Title: matchingDoc.data().Project_Title,
+              });
+            }
+          }
         });
-        
-        setTeamData(documents);
-        setLoading(false); 
+    
+        setData(combinedData); // Set the Data state with combinedData
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
     
     fetchData();
-
     const fetchMentors = async () => {
     
       const mentorsCollectionRef = collection(db, 'Staffs');
@@ -41,16 +60,13 @@ export default function ViewTeams() {
         const querySnapshot = await getDocs(mentorsCollectionRef);
         const mentorList = querySnapshot.docs.map((doc) => doc.data());
         setMentors(mentorList);
+        
       } catch (error) {
         console.error('Error fetching mentors:', error);
       }
     };
 
     fetchMentors();
-
-
-     
-
 
   }, []);
 
@@ -78,7 +94,7 @@ export default function ViewTeams() {
   if (loading) {
     return <div>Loading...</div>; 
   }
-  console.log(teamData);
+  
 
  
 
@@ -88,19 +104,15 @@ export default function ViewTeams() {
     <h1 className='teams'>Teams</h1>
     <div className="divider"></div> 
     <div className="app-container">
-
-      <div className="team-column">
-   
+    <div className="team-column">
     <div className="team-cards">
    
-    {teamData.map((doc, index) => (
-      
-            <Card key={index} title={`Team Leader: ${doc.Team_Leader_firstname}`}>
-              <p><strong>Mail ID:</strong> <br></br>{doc.Team_Leader_Mailid}</p>
-              <p><strong>Registration Number:</strong> {doc.Team_Leader_Regnum}</p>
-              <p><strong>Project Title:</strong> {doc.project_Title}</p>
-              <p><strong>Domain:</strong> {doc.Domain}</p>
-              
+    {Data.map(item => (
+        <Card key={item} title={`Team Leader: ${item.Team_Leader_firstname}`}>
+        <p>Registration Number: {item.Team_Leader_Regnum}</p>
+        <p>Mail ID: <br></br>{item.Team_Leader_Mailid}</p>
+        <p>Project Title: {item.Project_Title}</p>
+        <p>Project Title: {item.Domain}</p>
               <Select
                 placeholder="Select a mentor"
                 onChange={(selectedMentor) => setSelectedMentor(selectedMentor)}
